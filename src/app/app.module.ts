60cd98c37868b48a52157fbc13df39ffaef18e82
AhmedTroudi <ahmed.troudi@etudiant-fst.utm.tn>
2019-01-01 14:37:23 +0100

    adding auth services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FollowComponent } from './follow/follow.component';
+import { PopularComponent } from './popular/popular.component';
+import { IconboxesComponent } from './iconboxes/iconboxes.component';
+import { SearchComponent } from './search/search.component';
+import { SliderComponent } from './slider/slider.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+
+import { FormsModule} from '@angular/forms';
+import { httpInterceptorProviders } from './auth/auth-interceptor';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+    GalleryComponent,
+    FollowComponent,
+    PopularComponent,
+    IconboxesComponent,
+    SearchComponent,
+    SliderComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
