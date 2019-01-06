cb7f7e407cf2439542fc862956b031d67747a132
AhmedTroudi <ahmed.troudi@etudiant-fst.utm.tn>
2019-01-06 13:53:29 +0100

    minor changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {TokenStorageService} from '../auth/token-storage.service';
+import {AuthLoginInfo} from '../auth/login-info';
+import {AuthService} from '../auth/auth.service';
+import {User} from '../models/user';
 declare const M;
+const USERNAME_KEY = 'AuthUsername';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,12 +13,23 @@ declare const M;
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() {
+  isLoggedIn = false;
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
+  users: Array<User>;
+  username: string;
+
+  constructor(private tokenStorage: TokenStorageService, private authService: AuthService,) {
   }
 
   ngOnInit() {
     const sideNav = document.querySelector('.sidenav');
     M.Sidenav.init(sideNav, {});
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+      this.username = this.tokenStorage.getUsername();
+    }
   }
 }
 
